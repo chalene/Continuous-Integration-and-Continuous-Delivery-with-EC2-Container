f25af192b3579e76b0c1fc663473a7d9a698202e
AnnieYJ Lin <annie.lin@ecloudvalley.com>
2018-06-22 16:15:31 +0800

    Add files via upload

---
+++ ECS-101-DevOps_with_EC2_Container/RunFirst.sh
@@ -0,0 +1,8 @@
+﻿git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/workshop-commit
+cp -R ./Continuous-Integration-and-Continuous-Delivery-with-EC2-Container/ECS-101-DevOps_with_EC2_Container/* ./workshop-commit
+rm -rf Continuous-Integration-and-Continuous-Delivery-with-EC2-Container
+cd workshop-commit
+rm -rf images
+git add .
+git commit -m “workshop-bash”
+git push
